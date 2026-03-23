1502b8e24fdaf589f0744100caa86ca9c0b747a1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 15:14:36 -0400

    Update ScanSoftware.ps1

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -4,8 +4,8 @@ $web1 = "Google Chrome"
 
 $PC = $env:computername
 Write-Host '                                                            ' -BackgroundColor White
-Write-Host  "The following software is installed on:" "$PC"                    -ForegroundColor Blue -BackgroundColor White
-Write-Host        "Web Browser:" "$web1"                                       -ForegroundColor Green -BackgroundColor White
+Write-Host ' The following software is installed on: "$PC"              ' -ForegroundColor Blue -BackgroundColor White
+Write-Host '   Web Browser: "$web1"                                     ' -ForegroundColor Green -BackgroundColor White
 Write-Host '                                                            ' -BackgroundColor White
 }
 
