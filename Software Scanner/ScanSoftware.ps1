1bb72ee813410db52f96392efac4b0c6c131f660
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 14:53:22 -0400

    Update output messages in Show-FoundSoftware function

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -1,9 +1,11 @@
 Function Show-FoundSoftware(){
 $web1 = "Google Chrome"
 
+
+$PC = $env:computername
 Write-Host '                                                            ' -BackgroundColor White
-Write-Host ' The following software is installed on $env:computername : ' -ForegroundColor Blue -BackgroundColor White
-Write-Host ' "Web Browser:" $web1                                       ' -ForegroundColor Green -BackgroundColor White
+Write-Host ' The following software is installed on: $PC                ' -ForegroundColor Blue -BackgroundColor White
+Write-Host ' Web Browser: $web1                                       ' -ForegroundColor Green -BackgroundColor White
 Write-Host '                                                            ' -BackgroundColor White
 }
 
