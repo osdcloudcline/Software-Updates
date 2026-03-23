b8dfd726b67a14f38e018fda08a7ac35f8a32651
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 14:36:46 -0400

    Update ScanSoftware.ps1

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -1,20 +1,20 @@
 Function Show-FoundSoftware(){
 $web1 = "Google Chrome"
 
-Write-Host '                                                         ' -BackgroundColor White
-Write-Host ' The following software was found on $env:computername : ' -ForegroundColor Blue -BackgroundColor White
-Write-Host ' "" $web1                                                ' -ForegroundColor Green -BackgroundColor White
-Write-Host '                                                         ' -BackgroundColor White
+Write-Host '                                                            ' -BackgroundColor White
+Write-Host ' The following software is installed on $env:computername : ' -ForegroundColor Blue -BackgroundColor White
+Write-Host ' "Web Browser:" $web1                                       ' -ForegroundColor Green -BackgroundColor White
+Write-Host '                                                            ' -BackgroundColor White
 }
 
 
 Function Show-NotDetecteddSoftware(){
 $web1 = "Google Chrome"
 
-Write-Host '                                                             ' -BackgroundColor White
-Write-Host ' The following software was NOT found on $env:computername : ' -ForegroundColor Red -BackgroundColor White
-Write-Host ' "" $web1                                                    ' -ForegroundColor Green -BackgroundColor White
-Write-Host '                                                             ' -BackgroundColor White
+Write-Host '                                                                ' -BackgroundColor White
+Write-Host ' The following software is NOT installed on $env:computername : ' -ForegroundColor Red -BackgroundColor White
+Write-Host ' "Web Browser:" $web1                                           ' -ForegroundColor Green -BackgroundColor White
+Write-Host '                                                                ' -BackgroundColor White
 }
 
 
