47b207b831149244511aa45c94631b44022c8454
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-22 23:52:55 -0400

    Add color to output messages in UpdateADVBrowsers.ps1

--- Browsers/UpdateADVBrowsers.ps1
+++ Browsers/UpdateADVBrowsers.ps1
@@ -8,15 +8,15 @@ $GoogleEXE = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
 $GoogleTP = (Test-Path -Path $GoogleEXE -IsValid)
 
-Write-Host "Scanning for: $GoogleEXE" 
+Write-Host "Scanning for: $GoogleEXE" -ForegroundColor Cyan
 
 If($GoogleTP -eq $true){
-Write-Host "Processing updates for: $web1.."
+Write-Host "Processing updates for: $web1" -ForegroundColor Cyan 
 winget upgrade --id Google.Chrome
-Write-Host "Completed: $web1 update"
+Write-Host "Completed: $web1 update" -ForegroundColor Green
 }
 elseif($GoogleTP -eq $false){
-Write-Host "Processing: $web1 installation"
+Write-Host "Processing: $web1 installation" -ForegroundColor Cyan
 winget install --id Google.Chrome
-Write-Host "Completed: $web1 installation"
+Write-Host "Completed: $web1 installation" -ForegroundColor Green
 }
