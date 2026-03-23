0b03cc09587811431efaa233d822d3e7ea26acc4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 14:29:31 -0400

    Refactor software detection functions in ScanSoftware.ps1

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -1,6 +1,26 @@
-# Web Browsers region 
+Function Show-FoundSoftware(){
+$web1 = "Google Chrome"
+
+Write-Host '                                                         ' -BackgroundColor White
+Write-Host ' The following software was found on $env:computername : ' -ForegroundColor Blue -BackgroundColor White
+Write-Host ' "$web1"                                                   ' -ForegroundColor Green -BackgroundColor White
+Write-Host '                                                         ' -BackgroundColor White
+}
+
+
+Function Show-NotDetecteddSoftware(){
+$web1 = "Google Chrome"
+
+Write-Host '                                                             ' -BackgroundColor White
+Write-Host ' The following software was NOT found on $env:computername : ' -ForegroundColor Red -BackgroundColor White
+Write-Host ' "$web1"                                                       ' -ForegroundColor Green -BackgroundColor White
+Write-Host '                                                             ' -BackgroundColor White
+}
+
 
 Function Show-ScanSoftware(){
+
+# Web Browsers region 
 
 # Google Chrome region
 
@@ -16,26 +36,6 @@ Show-NotDetectedSoftware
 }
 
 
-Function Show-FoundSoftware(){
-$web1 = "Google Chrome"
 
-
-
-Write-Host '                                                         ' -BackgroundColor White
-Write-Host ' The following software was found on $env:computername : ' -ForegroundColor Blue -BackgroundColor White
-Write-Host ' $web1                                                   ' -ForegroundColor Green -BackgroundColor White
-Write-Host '                                                         ' -BackgroundColor White
-}
-
-
-Function Show-NotDetecteddSoftware(){
-$web1 = "Google Chrome"
-
-
-Write-Host '                                                             ' -BackgroundColor White
-Write-Host ' The following software was NOT found on $env:computername : ' -ForegroundColor Red -BackgroundColor White
-Write-Host ' $web1                                                       ' -ForegroundColor Green -BackgroundColor White
-Write-Host '                                                             ' -BackgroundColor White
-}
 
 Show-ScanSoftware
