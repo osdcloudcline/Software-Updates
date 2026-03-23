81c02621882075e70e1bdf9ad1b4cb3592fe8863
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 14:20:55 -0400

    Rename function to Show-ScanSoftware and update logic

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -1,11 +1,36 @@
 # Web Browsers region 
 
-Function Show-ScanBrowsers(){
+Function Show-ScanSoftware(){
 
 # Google Chrome region
 
+$web1 = "Google Chrome"
 $GoogleEXE = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 $GoogleTP = (Test-Path -Path $GoogleEXE -IsValid)
 
+If($GoogleTP -eq $true){
+Show-FoundSoftware
+ElseIf($GoogleTP -eq $false)
+Show-NotDetectedSoftware 
+}
+}
+
+
+Function Show-FoundSoftware(){
+
+Write-Host'                                                          ' -BackgroundColor White
+Write-Host ' The following software was found on $env:computername : ' -ForegroundColor Blue -BackgroundColor White
+Write-Host ' $web1                                                   ' -ForegroundColor Green -BackgroundColor White
 
 }
+
+
+Function Show-NotDetecteddSoftware(){
+
+Write-Host'                                                        '     -BackgroundColor White
+Write-Host ' The following software was NOT found on $env:computername : -ForegroundColor Red -BackgroundColor White
+Write-Host ' $web1
+
+}
+
+Show-ScanSoftware
