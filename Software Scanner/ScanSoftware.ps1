59af8350a92fed70c5ee2f1e5fd5d478b0a56e1b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 14:56:22 -0400

    Fix quotes in output for web browser display

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -5,7 +5,7 @@ $web1 = "Google Chrome"
 $PC = $env:computername
 Write-Host '                                                            ' -BackgroundColor White
 Write-Host ' The following software is installed on: $PC                ' -ForegroundColor Blue -BackgroundColor White
-Write-Host ' Web Browser: $web1                                       ' -ForegroundColor Green -BackgroundColor White
+Write-Host ' "Web Browser: $web1                                       "' -ForegroundColor Green -BackgroundColor White
 Write-Host '                                                            ' -BackgroundColor White
 }
 
@@ -15,7 +15,7 @@ $web1 = "Google Chrome"
 
 Write-Host '                                                                ' -BackgroundColor White
 Write-Host ' The following software is NOT installed on $env:computername : ' -ForegroundColor Red -BackgroundColor White
-Write-Host ' Web Browser: $web1                                             ' -ForegroundColor Green -BackgroundColor White
+Write-Host ' "Web Browser: $web1                                           "' -ForegroundColor Green -BackgroundColor White
 Write-Host '                                                                ' -BackgroundColor White
 }
 
