0371dee383a104f11f1e87321d442cf38cf2adf8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 14:09:52 -0400

    Implement Show-ScanBrowsers function
    Added function to check for Google Chrome installation

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -1,3 +1,11 @@
 # Web Browsers region 
 
+Function Show-ScanBrowsers(){
 
+# Google Chrome region
+
+$GoogleEXE = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$GoogleTP = (Test-Path -Path $GoogleEXE -IsValid)
+
+
+}
