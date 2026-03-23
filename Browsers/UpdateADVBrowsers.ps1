9de8bb5971260cf24b6a10daa02e63878c96ed78
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-22 23:49:55 -0400

    Add Google Chrome update and installation logic

--- Browsers/UpdateADVBrowsers.ps1
+++ Browsers/UpdateADVBrowsers.ps1
@@ -1,1 +1,22 @@
 
+# Web Browser variables region
+
+$web1 = "Google Chrome"
+
+
+$GoogleEXE = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+$GoogleTP = (Test-Path -Path $GoogleEXE -IsValid)
+
+Write-Host "Scanning for: $GoogleEXE" 
+
+If($GoogleTP -eq $true){
+Write-Host "Processing updates for: $web1.."
+winget upgrade --id Google.Chrome
+Write-Host "Completed: $web1 update"
+}
+elseif($GoogleTP -eq $false){
+Write-Host "Processing: $web1 installation"
+winget install --id Google.Chrome
+Write-Host "Completed: $web1 installation"
+}
