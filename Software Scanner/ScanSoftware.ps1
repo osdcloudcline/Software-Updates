7ab0dd7f7a1cd45813fa4e86e3dd62cc7cccb895
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 14:33:16 -0400

    Fix string interpolation in Write-Host commands

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -3,7 +3,7 @@ $web1 = "Google Chrome"
 
 Write-Host '                                                         ' -BackgroundColor White
 Write-Host ' The following software was found on $env:computername : ' -ForegroundColor Blue -BackgroundColor White
-Write-Host ' "$web1"                                                   ' -ForegroundColor Green -BackgroundColor White
+Write-Host ' "" $web1                                                ' -ForegroundColor Green -BackgroundColor White
 Write-Host '                                                         ' -BackgroundColor White
 }
 
@@ -13,7 +13,7 @@ $web1 = "Google Chrome"
 
 Write-Host '                                                             ' -BackgroundColor White
 Write-Host ' The following software was NOT found on $env:computername : ' -ForegroundColor Red -BackgroundColor White
-Write-Host ' "$web1"                                                       ' -ForegroundColor Green -BackgroundColor White
+Write-Host ' "" $web1                                                    ' -ForegroundColor Green -BackgroundColor White
 Write-Host '                                                             ' -BackgroundColor White
 }
 
@@ -30,7 +30,8 @@ $GoogleTP = (Test-Path -Path $GoogleEXE -IsValid)
 
 If($GoogleTP -eq $true){
 Show-FoundSoftware
-elseif($GoogleTP -eq $false)
+}
+elseif($GoogleTP -eq $false){
 Show-NotDetectedSoftware 
 }
 }
