a9053956a1559ce58de4eaef4c91c95dfae1ad59
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-22 23:55:10 -0400

    Add additional logging for Google Chrome updates

--- Browsers/UpdateADVBrowsers.ps1
+++ Browsers/UpdateADVBrowsers.ps1
@@ -9,14 +9,18 @@ $GoogleEXE = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 $GoogleTP = (Test-Path -Path $GoogleEXE -IsValid)
 
 Write-Host "Scanning for: $GoogleEXE" -ForegroundColor Cyan
-
+Write-Host
 If($GoogleTP -eq $true){
 Write-Host "Processing updates for: $web1" -ForegroundColor Cyan 
+Write-Host
 winget upgrade --id Google.Chrome
 Write-Host "Completed: $web1 update" -ForegroundColor Green
+Write-Host
 }
 elseif($GoogleTP -eq $false){
 Write-Host "Processing: $web1 installation" -ForegroundColor Cyan
+Write-Host
 winget install --id Google.Chrome
 Write-Host "Completed: $web1 installation" -ForegroundColor Green
+Write-Host
 }
