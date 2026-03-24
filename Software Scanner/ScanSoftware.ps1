21d007baf837c4257c4e4a25a1d1b109343095f7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 21:36:30 -0400

    Update ScanSoftware.ps1

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -6,8 +6,8 @@ $Cat1ArgList = '"{0}" "{1}"' -f $category1, $web1
 
 $PC = $env:computername
 Write-Host '                                                                  ' -BackgroundColor White
-Write-Host '     The following software is installed on: "$PC"                ' -ForegroundColor Blue -BackgroundColor White
-Write-Host '            "$Cat1ArgList"                                        ' -ForegroundColor Green -BackgroundColor White
+Write-Host "     The following software is installed on: $PC                  " -ForegroundColor Blue -BackgroundColor White
+Write-Host "            $Cat1ArgList                                          " -ForegroundColor Green -BackgroundColor White
 Write-Host '                                                                  ' -BackgroundColor White
 }
 
@@ -16,10 +16,10 @@ Function Show-NotDetecteddSoftware(){
 $category1 = "Web Browsers:"
 $web1 = "Google Chrome"
 
-Write-Host '                                                                ' -BackgroundColor White
-Write-Host ' The following software is NOT installed on $env:computername : ' -ForegroundColor Red -BackgroundColor White
-Write-Host ' "Web Browser: $web1                                            ' -ForegroundColor Green -BackgroundColor White
-Write-Host '                                                                ' -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White
+Write-Host  "The following software is NOT installed on $env:computername :   " -ForegroundColor Red -BackgroundColor White
+Write-Host  "Web Browser: $web1                                              " -ForegroundColor Green -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White
 }
 
 
