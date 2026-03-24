0e211c0bd46bf7eb0b3a4392d5c74bdf5b32a358
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-23 21:44:10 -0400

    Update ScanSoftware.ps1

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -7,7 +7,7 @@ $web1 = "Google Chrome"
 $PC = $env:computername
 
 Write-Host '                                                                  ' -BackgroundColor White
-Write-Host " The Current Windows hostname is : $PC                            " -ForegroundColor Blue -BackgroundColor White
+Write-Host ' "The Current Windows hostname is:" "$PC"                         ' -ForegroundColor Blue -BackgroundColor White
 Write-Host '                                                                  ' -BackgroundColor White
 Write-Host '     The following software is installed:                         ' -ForegroundColor Blue -BackgroundColor White
 Write-Host '                                                                  ' -ForegroundColor Green -BackgroundColor White
