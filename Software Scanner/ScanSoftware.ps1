a72e708897495e373ab535d189b094f3a43d3080
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-03-24 14:10:17 -0400

    Update ScanSoftware.ps1

--- Software Scanner/ScanSoftware.ps1
+++ Software Scanner/ScanSoftware.ps1
@@ -1,8 +1,8 @@
-Function Show-FoundSoftware(){
+Function Show-SoftwareSummary(){
 
 $category1 = "Web Browsers:"
 $web1 = "Google Chrome"
-
+$web2 = "Mozilla Firefox"
 
 $PC = $env:computername
 
@@ -13,12 +13,12 @@ Write-Host '                                                                  ' 
 Write-Host '     The following software is installed:                         ' -ForegroundColor Blue -BackgroundColor White
 Write-Host '                                                                  ' -ForegroundColor Green -BackgroundColor White
 Write-Host '                                                                  ' -BackgroundColor White
-}
 
 
-Function Show-NotDetecteddSoftware(){
 $category1 = "Web Browsers:"
 $web1 = "Google Chrome"
+$web2 = "Mozilla Firefox"
+
 
 Write-Host '                                                                 ' -BackgroundColor White
 Write-Host  "The following software is NOT installed on $env:computername :   " -ForegroundColor Red -BackgroundColor White
@@ -27,7 +27,79 @@ Write-Host '                                                                 ' -
 }
 
 
+Function Get-SoftwareScan {
+ param([string]$SoftwareScan
+
+ $status = @{
+ Name = $SoftwareScan
+ IsNotInstalled = $false
+ IsInstalled = $true
+ }
+
+switch ($SoftwareScan){
+ "Google Chrome"{
+ }
+ 
+
 Function Show-ScanSoftware(){
+
+#Software to Scan
+
+$SoftwareScan = @(
+"Google Chrome",
+"Mozilla Firefox",
+"DuckDuck Go",
+"DropBox",
+"Google Cloud - GDrive",
+"NextCloud Desktop Client",
+"Microsoft OneDrive",
+"Apple iCloud",
+"Custom UPBR",
+"Docker Desktop",
+"GitHub Desktop",
+"FileZilla",
+"PuTTY",
+"WinSCP",
+"Steam",
+"Audacity",
+"FFMPEG",
+"GIMP",
+"Handbrake",
+"Kodi",
+"MKVToolNix",
+"MakeMKV",
+"MediaInfo",
+"Logitech My Harmony",
+"PowerISO",
+"VLC Player",
+"iTunes",
+"Acrobat Reader",
+"AI Cursor Editor",
+"Visual Studio 2026 Enterprise",
+"VS Code",
+"Crucial Storage",
+"Logitech Options",
+"Logitech Unifying Receiver Software",
+"Microsoft Mouse and Keyboard",
+"Samsung Magician",
+"Western Digital SSD",
+"7-Zip",
+"CCleaner",
+"Display Driver Uninstaller",
+"Smart Defrag",
+"Wise Registry Cleaner",
+"Any Desk Remote Desktop VDI Client",
+"Microsoft Remote Desktop",
+"VMWare Horizon",
+"VMWare Workstation Pro"
+)
+
+# Scan software
+$AllSoftware = @()
+ foreach ($SoftwareName in $SoftwareScan){
+  Write-Host "Checking $SoftwareName..." -ForegroundColor Gray
+  $status = Get-SoftwareScan $SoftwareName
+  $AllSoftware += $status
 
 # Web Browsers region 
 
@@ -37,8 +109,14 @@ $web1 = "Google Chrome"
 $GoogleEXE = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 $GoogleTP = (Test-Path -Path $GoogleEXE -IsValid)
 
-If($GoogleTP -eq $true){
-Show-FoundSoftware
+# Firefox region
+
+$web2 = "Mozilla Firefox"
+$FirefoxEXE = ""
+$FirefoxTP = ""
+
+If($GoogleTP -eq $true) -and ($FirefoxTP -eq $true){
+Show-SoftwareSummary
 }
 elseif($GoogleTP -eq $false){
 Show-NotDetectedSoftware 
