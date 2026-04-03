729f03273771fc180a25d694e6f3cb2d7012a73e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-02 23:32:21 -0400

    Update and rename UpdateADVBrowsers.ps1 to UpdateBrowsers.ps1

---
+++ Browsers/UpdateBrowsers.ps1
@@ -0,0 +1,30 @@
+# Define the browsers in a simple list (Array of Objects)
+$Browsers = @(
+    @{ Name = "Google Chrome"; Path = $GoogleEXE; ID = "Google.Chrome" }
+    @{ Name = "Mozilla Firefox"; Path = $FirefoxEXE; ID = "Mozilla.Firefox" }
+    @{ Name = "Duck Duck Go"; Path = $DuckDuckGoEXE; ID = "DuckDuckGo.DesktopBrowser" }
+)
+
+$AnyFound = $false
+
+foreach ($App in $Browsers) {
+    if (Test-Path -Path $App.Path) {
+        $AnyFound = $true
+        Write-Host "$($App.Name) is installed on $env:computername at $($App.Path)" -ForegroundColor DarkBlue -BackgroundColor White
+        Write-Host "`nProcessing updates for: $($App.Name)" -ForegroundColor Cyan
+        
+        winget upgrade --id $App.ID
+        
+        Write-Host "Completed: $($App.Name) update`n" -ForegroundColor Green
+    } else {
+        Write-Host "$($App.Name) is NOT installed on $env:computername" -ForegroundColor Red -BackgroundColor White
+    }
+}
+
+if (-not $AnyFound) {
+    Write-Warning "No third-party browsers are installed!"
+}
+
+$UpdateSoftwareMain = Invoke-WebRequest("")
+Invoke-Expression $($UpdateSoftwareMain.Content)
+
