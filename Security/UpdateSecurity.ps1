134de6e8397e1324c7e06a7d0bdbc73630946ba9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-04 07:58:31 -0400

    Update UpdateSecurity.ps1

--- Security/UpdateSecurity.ps1
+++ Security/UpdateSecurity.ps1
@@ -1,1 +1,28 @@
+# Security file paths
+      $MBAMEXE = "C:\Program Files\Adobe\Acrobat DC\Acrobat\ReaderMenuCoreApp.msix" 
+      
 
+# Define the security software in a simple list (Array of Objects)
+$AntiMalware = @(
+    @{ Name = "MalwareBytes Anti-Malware"; Path = $MBAMEXE; ID = "Malwarebytes.Malwarebytes" }
+)
+
+$AnyFound = $false
+
+foreach ($App in $AntiMalware) {
+    if (Test-Path -Path $App.Path) {
+        $AnyFound = $true
+        Write-Host "$($App.Name) is installed on $env:computername at $($App.Path)" -ForegroundColor DarkBlue -BackgroundColor White
+        Write-Host "`nProcessing updates for: $($App.Name)" -ForegroundColor Cyan
+        
+        winget upgrade --id $App.ID
+        
+        Write-Host "Completed: $($App.Name) update`n" -ForegroundColor Green
+    } else {
+        Write-Host "$($App.Name) is NOT installed on $env:computername" -ForegroundColor Red -BackgroundColor White
+    }
+}
+
+if (-not $AnyFound) {
+    Write-Warning "No Anti-Malware software is installed!"
+}
